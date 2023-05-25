be0b744c0e9a4ee5c2d9d7247fcf31c951d225f0
Jimin Baek <qorwlalsjimin@naver.com>
2023-05-25 09:57:40 +0900

    피보나치 수열

---
+++ RecursionPrac32/Fibonacci.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RecursionPrac32
+{
+    internal class Fibonacci
+    {
+        public long Get(int n)
+        {
+            //Get(3) -> Get(2) + Get(1)
+            //Console.WriteLine(n);
+           if(n<0) { return 0;  }
+           if(n==1) { return 1; }   
+           return Get(n-2)+Get(n-1);
+        }
+    }
+}
